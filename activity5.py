e839ddbc2c69bbc451b4adda31dd3d148d2d3d5e
markamillerr <mill1102@bears.unco.edu>
2025-11-03 19:03:45 +0000

    draft

--- activity5.py
+++ activity5.py
@@ -13,11 +13,31 @@ wordsfile= open('CROSSWD.txt','r')
 #       print(k)
 # print ([x for x in dir(wordsfile) if 'read' in x])
 
-def twenty_or_more(file):
-    result= []
-    for i in file:
-        if file[i] > 20:
-            result.append(file[i])
-    return result
+def more_than_20(file):
+    wrds= open(file)
+    ret_list= []
+    for i in wrds:
+        x= i.strip()
+        if len(x)> 20:
+            ret_list.append(x)
+    return ret_list
 
-print(twenty_or_more(wordsfile))
+# print(more_than_20('CROSSWD.txt'))
+
+def has_no_e(word):    
+    if 'e' in word:
+        return False 
+    else:
+        return True
+
+# print(has_no_e('allegory'))
+
+def uses_only(word, letters):
+    # temp = letters
+    for letter in word:
+        if letter not in letters:
+            return False
+    return True  
+           
+#print(uses_only('abra','abr'))
+    
